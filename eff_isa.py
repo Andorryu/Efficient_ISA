96dcc2c46225be8b54fd6cfe9b5f6af502598aa1
andorryu <adority121@gmail.com>
2023-11-20 11:29:58 -0600

    basic

---
+++ eff_isa.py
@@ -0,0 +1,8 @@
+
+from sys import argv
+
+def main(argv: list[str]) -> None:
+    
+
+if __name__ == "__main__":
+    main(argv)
---
+++ operation.py
@@ -0,0 +1,4 @@
+
+class operation:
+    def __init__(self) -> None:
+        pass
